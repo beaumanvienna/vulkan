9eab4cf74b1f3e833d8c3b155b18c0d30f2c718e
beaumanvienna (JC) <beaumanvienna@gmail.com>
2022-01-17 18:29:22 -0800

    maintenance

--- scripts/compileShader.sh
+++ scripts/compileShader.sh
@@ -1,3 +1,4 @@
 #/bin/bash
+mkdir -p bin
 glslc engine/platform/Vulkan/shader/simpleShader.vert -o bin/simpleShader.vert.spv
 glslc engine/platform/Vulkan/shader/simpleShader.frag -o bin/simpleShader.frag.spv
